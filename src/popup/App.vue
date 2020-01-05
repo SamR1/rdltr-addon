b1b4f2c0520a4be0328204a56197bd21fd2ebfb2
Sam <sam@samr1.net>
2020-01-05 10:42:27 +0100

    display if connected to rdltr instance in popup

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -4,7 +4,14 @@
       <div class="logo">rdltr <small>a simple "read-it later" app</small></div>
     </header>
     <div class="rdltr">
-      <button type="submit">Add current tab to <strong>rdltr</strong></button>
+      <div v-if="authToken">
+        <button type="submit">Add current tab to <strong>rdltr</strong></button>
+      </div>
+      <div v-else>
+        <span class="rdltr-error"
+          >Not connected to an <strong>rdltr</strong> instance</span
+        >
+      </div>
     </div>
   </div>
 </template>
@@ -12,7 +19,21 @@
 <script>
 export default {
   data() {
-    return {}
+    return {
+      authToken: null,
+      error: null,
+      url: null,
+      user: null,
+    }
+  },
+  mounted() {
+    const gettingAllStorageItems = browser.storage.local.get(null)
+    gettingAllStorageItems.then(results => {
+      const itemsKeys = Object.keys(results)
+      for (const itemKey of itemsKeys) {
+        this[itemKey] = results[itemKey]
+      }
+    })
   },
 }
 </script>
